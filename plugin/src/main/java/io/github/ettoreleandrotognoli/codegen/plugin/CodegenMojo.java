23ab3e0c50946921f37c1f24c0f6773c4df5aaca
Ettore Leandro Tognoli <eleandro@redhat.com>
2020-05-15 12:58:59 +0200

    plugin

--- plugin/src/main/java/io/github/ettoreleandrotognoli/codegen/plugin/CodegenMojo.java
+++ plugin/src/main/java/io/github/ettoreleandrotognoli/codegen/plugin/CodegenMojo.java
@@ -1,5 +1,7 @@
 package io.github.ettoreleandrotognoli.codegen.plugin;
 
+import io.github.ettoreleandrotognoli.codegen.api.Project;
+import io.github.ettoreleandrotognoli.codegen.core.CodegenEngine;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -9,6 +11,11 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.stream.Stream;
 
 @Mojo(name = "codegen", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class CodegenMojo extends AbstractMojo {
@@ -21,10 +28,19 @@ public class CodegenMojo extends AbstractMojo {
     File outputDirectory;
 
     @Parameter(defaultValue = "${project.baseDir}/src/main/codegen/main.yml")
-    File[] codegenFiles;
+    File codegenFiles;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        
+        Stream<InputStream> input = Collections.singletonList(codegenFiles).stream().map(file -> {
+            try {
+                return (InputStream) new FileInputStream(file);
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+                return null;
+            }
+        }).filter(it -> it != null);
+        Project project = new Project.DTO(this.project.getBasedir());
+        CodegenEngine.Companion.getInstance().process(project, input);
     }
 }
