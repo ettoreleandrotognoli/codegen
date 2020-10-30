54ed6e7b9abb7fa2bcf04c566d122720f63eb16d
Ettore Leandro Tognoli <eleandro@redhat.com>
2020-10-30 15:55:07 +0100

    avoid backup pom

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 VERSION=$1
 
 git checkout -b $1
-mvn versions:set -DnewVersion=$1
+mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
 git add .
 git commit -m "bump version to $1"
 git tag $1
